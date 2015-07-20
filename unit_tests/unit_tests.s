92fa8821745b9fc45600d4d0aac10e084d20a11b
Marcus Rowe <undisbeliever@gmail.com>
2015-07-20 21:17:15 +1000

    fix: unit tests would not compile.

--- unit_tests/unit_tests.s
+++ unit_tests/unit_tests.s
@@ -198,11 +198,11 @@ ROUTINE SetupPPU
 .rodata
 
 Font8x8BoldTransparent:
-	.incbin "../resources/font8x8-bold-transparent.2bpp"
+	.incbin "../resources/tiles2bpp/font8x8-bold-transparent.2bpp"
 Font8x8BoldTransparent_End:
 
 Font8x16BoldTransparent:
-	.incbin "../resources/font8x16-bold-transparent.2bpp"
+	.incbin "../resources/tiles2bpp/font8x16-bold-transparent.2bpp"
 Font8x16BoldTransparent_End:
 
 FontBoldTransparentPalette:                ; ANSI Colors
