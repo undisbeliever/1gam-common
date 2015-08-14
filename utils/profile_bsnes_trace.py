8c41de08d45ae9a694c4bb00b4b42c97df9e1e7d
Marcus Rowe <undisbeliever@gmail.com>
2015-08-14 22:32:57 +1000

    feat: handle interrupts and WAIs

--- utils/profile_bsnes_trace.py
+++ utils/profile_bsnes_trace.py
@@ -6,9 +6,6 @@
 RoutineProfiles a bsnes assembly trace.
 """
 
-# ::TODO read memlog file::
-# ::TODO handle interrupts::
-# ::TODO determine CPU utilisation by use of Screen__WaitFrame function::
 # ::SHOULDO CPU usage per frame::
 
 import re
@@ -19,7 +16,14 @@ import os.path
 
 SNES_SCANLINES = 262
 SNES_HTIME     = 1374
-
+COMMON_INTERRUPT_NAMES = (
+        'VBLANK', 'VBlank', 'NmiHandler', 'NMI',
+        'IrqHandler', 'IRQ',
+        'CopHandler', 'COP',
+        'ResetHandler', 'RESET',
+        'BreakHandler', 'BREAK',
+        'EmptyHandler',
+)
 
 class LineReader:
     """
@@ -27,6 +31,8 @@ class LineReader:
     """
     def __init__(self, fp):
         self.fp = fp
+        self.line = None
+        self.lineno = 0
         self.peek = fp.readline()
 
     def __iter__(self):
@@ -36,6 +42,8 @@ class LineReader:
         line = self.peek
 
         if line:
+            self.line = line
+            self.lineno += 1
             self.peek = self.fp.readline()
             return line
         else:
@@ -61,12 +69,30 @@ class MemoryMap:
                         addr = int(m.group(4), 16) & 0x7FFFFF
                         self.addresses[addr] = m.group(3)
 
+    def nameAddressIfUnlabled(self, addr, name):
+        addr = addr & 0x7FFFFF
+        if addr not in self.addresses:
+            self.addresses[addr] = name
+
     def nameForAddress(self, addr):
         maddr = addr & 0x7FFFFF
         if maddr in self.addresses:
             return self.addresses[maddr]
         else:
             return None
+
+    def addressForName(self, name):
+        for a, n in self.addresses.items():
+            if n == name:
+                return a
+        return -1
+
+    def nameOrAddress(self, addr):
+        maddr = addr & 0x7FFFFF
+        if maddr in self.addresses:
+            return self.addresses[maddr]
+        else:
+            return '%06x' % addr
 
 
 
@@ -78,7 +104,7 @@ class RoutineProfile:
 
         self.count = 0
         self.time = 0
-        self.totaltime = -1
+        self.totaltime = 0
         self._calls = dict() # dict of tuple (callAddress, routineAddress) -> RoutineProfile
 
     def sorted_calls(self):
@@ -111,50 +137,91 @@ class RoutineTotal:
 
 
 class Profile:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, memmap):
+        self.memmap = memmap
+        self.name = None
         self.root = None
+        self.time = 0
+        self.waiTime = 0
+        self._interrupts = dict() # mapping (address) -> RoutineProfile
+        self.profiles = set()
 
 
-    def read_trace(self, fp):
+    def addInterrupts(self, *interrupts):
+        for i in interrupts:
+            try:
+                a = int(i, 16)
+            except:
+                a = self.memmap.addressForName(i)
+
+            if a >= 0:
+                rt = RoutineProfile(a, None, -1)
+                self._interrupts[a & 0x7FFFFF] = rt
+                self._interrupts[a | 0x800000] = rt
+
+
+    def readTrace(self, fp):
         # ::TODO add bsnes-plus Frame Counter (optional)::
         regex = re.compile(r'^([A-Za-z0-9]{6})\s+(\w+?)\s.+?V:\s*(\d+)\sH:\s*(\d+)')
         addr_regex = re.compile(r'^([A-Za-z0-9]{6})\s')
 
+        profiles = set() # mapping address -> RoutineProfile
+        interruptStack = list()
         reader = LineReader(fp)
 
         m = regex.match(reader.peek)
         if not m:
             raise ValueError("Not in a trace format")
 
-        root = RoutineProfile(int(m.group(1), 16), None, -1)
         prevVTime = int(m.group(3))
         prevHTime = int(m.group(4))
 
-        current = root
+
+        def time_since_last_line(m):
+            nonlocal prevVTime, prevHTime
+
+            vTime = int(m.group(3))
+            hTime = int(m.group(4))
+
+            if vTime < prevVTime:
+                vTime += SNES_SCANLINES
+
+            if prevVTime != vTime:
+                hTime += SNES_HTIME * (vTime - prevVTime)
+
+            time = hTime - prevHTime
+
+            prevVTime = int(m.group(3))
+            prevHTime = int(m.group(4))
+
+            return time
+
+
+        # First line may be VBlank, check first.
+        instAddr = int(m.group(1), 16)
+        if instAddr not in self._interrupts:
+            current = RoutineProfile(instAddr, None, -1)
+            profiles.add(current)
+        else:
+            # First line Interrupt;
+            current = None
+
+        # Mark first instruction as entry.
+        self.memmap.nameAddressIfUnlabled(instAddr, "__TRACE_START__")
 
         for line in reader:
             m = regex.match(line)
             if m:
-                # Calculate time between instructions.
-                vTime = int(m.group(3))
-                hTime = int(m.group(4))
-
-                if vTime < prevVTime:
-                    vTime += SNES_SCANLINES
-
-                if prevVTime != vTime:
-                    hTime += SNES_HTIME * (vTime - prevVTime)
-
-                current.time += hTime - prevHTime
-
-                prevVTime = int(m.group(3))
-                prevHTime = int(m.group(4))
-
-
-                # Determine if enter a routine
                 inst = m.group(2).lower()
                 instAddr = int(m.group(1), 16)
+
+                # Check to see if an interrupt occoured
+                if instAddr in self._interrupts:
+                    interruptStack.append(current)
+                    current = self._interrupts[instAddr]
+                    current.count += 1
+
+                current.time += time_since_last_line(m)
 
                 if inst == 'jsr' or inst == 'jsl':
                     # Determine the location of the routine by address of next line
@@ -165,21 +232,54 @@ class Profile:
                     p.count += 1
                     current = p
 
-                # Determine if exit a routine
-                if inst == 'rts' or inst == 'rtl':
+                elif inst == 'rts' or inst == 'rtl':
+                    # Return from previous instruction
+
                     if current.caller:
                         current = current.caller
                     else:
+                        # Returning to a routine that hasn't been traced before.
                         # Determine the location of the routine by address of next line
                         nm = addr_regex.match(reader.peek)
                         routineAddr = int(nm.group(1), 16)
 
+                        prev = current
                         current = RoutineProfile(routineAddr, None, -1)
-                        root.caller = current
-                        current.add_call(root)
-                        root = current
+                        prev.caller = current
+                        current.add_call(prev)
 
-        self.root = root
+                        profiles.discard(prev)
+                        profiles.add(current)
+
+                elif inst == 'wai':
+                    # Add time waiting for interrupt to current function, not the interrupt itself
+                    nm = regex.match(reader.peek)
+                    if nm:
+                        t = time_since_last_line(nm)
+                        current.time += t
+                        self.waiTime += t
+
+                elif inst == 'rti':
+                    # Return from interrupt
+                    try:
+                        current = interruptStack.pop()
+                    except IndexError:
+                        current = None
+
+                    if current == None:
+                        nm = addr_regex.match(reader.peek)
+                        returnAddr = int(nm.group(1), 16)
+
+                        current = RoutineProfile(returnAddr, None, -0xFF)
+                        profiles.add(current)
+
+
+        # Only add interrupts that were called
+        for i in self._interrupts.values():
+            if i.count > 0:
+                self.profiles.add(i)
+
+        self.profiles.update(profiles)
         self._calculate_total_time()
         self._generate_routines_list()
 
@@ -194,7 +294,9 @@ class Profile:
 
             return total
 
-        self.totaltime = recurse(self.root)
+        self.totaltime = 0
+        for p in self.profiles:
+            self.totaltime += recurse(p)
 
 
     def _generate_routines_list(self):
@@ -211,13 +313,16 @@ class Profile:
             for p in proutine.calls():
                 recurse(p)
 
-        recurse(self.root)
+        for p in self.profiles:
+            recurse(p)
+
         self.routines = sorted(routines.values(), key=lambda x: x.time, reverse=True)
 
 
 
-def write_routine_times(fp, profile, memmap):
+def write_routine_times(fp, profile):
     totaltime = profile.totaltime
+    memmap = profile.memmap
 
     fp.write('<h2>Routines:</h2>')
     fp.write('<table>')
@@ -249,7 +354,8 @@ def write_routine_times(fp, profile, memmap):
 
 
 
-def write_profile_calls(fp, profile, memmap):
+def write_profile_calls(fp, profile):
+    memmap = profile.memmap
 
     def recurse(proutine, parent_totaltime):
         fp.write("<li>")
@@ -282,21 +388,32 @@ def write_profile_calls(fp, profile, memmap):
                 recurse(p, proutine.totaltime)
             fp.write('</ul>')
 
-    fp.write('<h2>Profile:</h2>')
-    fp.write('<ul>')
 
-    recurse(profile.root, profile.totaltime)
+    for p in profile.profiles:
+        fp.write('<h2>%s:</h2>' % memmap.nameOrAddress(p.address))
+        fp.write('<ul>')
 
-    fp.write('</ul>')
+        recurse(p, p.totaltime)
+
+        fp.write('</ul>')
 
 
 
-def write_html(fp, profile, memmap):
-    fp.write('<html><title>%s tracelog profile</title><body>' % profile.name)
-    fp.write('<h1>%s</h1>' % profile.name)
+def write_html(fp, profile):
+    fp.write("<html><title>%s tracelog profile</title><body>" % profile.name)
+    fp.write("<h1>%s</h1>" % profile.name)
 
-    write_routine_times(fp, profile, memmap)
-    write_profile_calls(fp, profile, memmap)
+    p =  profile.waiTime / profile.totaltime * 100
+
+    fp.write("<div>%d Cycles Traced, %d WAI cycles (%f%% WAI, %f%% non WAI).</div>" % (
+        profile.totaltime,
+        profile.waiTime,
+        p,
+        100 - p,
+    ))
+
+    write_routine_times(fp, profile)
+    write_profile_calls(fp, profile)
 
     fp.write('</body></html>')
 
@@ -305,41 +422,56 @@ def write_html(fp, profile, memmap):
 def process_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-m', '--memlog', type=str,
+    parser.add_argument('-m', '--memlog',
+            type=str,
             help='Memlog file',
     )
+    parser.add_argument('-i', '--interrupt', metavar='INTERRUPT',
+            dest='interrupts', type=str,  action='append',
+            help="""Addresses / Names of interrupt. Can be used multiple times.
+If memlog is supplied and this argument is not used, then common names
+will be guessed from the memlog file."""
+    )
     parser.add_argument('logfile',
+            type=str,
             help='The SNES assembly trace file (`-` is stdin)',
     )
     parser.add_argument('htmlfile',
+            type=str,
             help='The HTML output file (`-` is stdout)',
     )
 
     return parser.parse_args()
 
 
+
 def main():
     args = process_args()
 
     memmap = MemoryMap()
+    profile = Profile(memmap)
 
     if args.memlog:
         memmap.load(args.memlog)
 
-    if args.logfile == '-':
-        profile = Profile('SNES')
-        profile.read_trace(sys.stdin)
+    if args.interrupts:
+        profile.addInterrupts(*args.interrupts)
     else:
-        name = os.path.splitext(os.path.basename(args.logfile))[0]
+        profile.addInterrupts(*COMMON_INTERRUPT_NAMES)
+
+    if args.logfile == '-':
+        profile.name = 'SNES'
+        profile.readTrace(sys.stdin)
+    else:
         with open(args.logfile, 'r') as fp:
-            profile = Profile(name)
-            profile.read_trace(fp)
+            profile.name = os.path.splitext(os.path.basename(args.logfile))[0]
+            profile.readTrace(fp)
 
     if args.htmlfile == '-':
-        write_html(sys.stdout, profile, memmap)
+        write_html(sys.stdout, profile)
     else:
         with open(args.htmlfile, 'w') as fp:
-            write_html(fp, profile, memmap)
+            write_html(fp, profile)
 
 
 if __name__ == '__main__':
